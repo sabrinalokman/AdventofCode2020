8b474f1c91aae8c66799586ddc3436efd8afb2eb
Sabrina Lokman <35489261+sabrinalokman@users.noreply.github.com>
2020-12-18 19:44:48 -0500

    Solve part 1

--- Day9/Day9.cpp
+++ Day9/Day9.cpp
@@ -4,100 +4,67 @@
 #include <sstream>
 #include <map>
 #include <utility>
-std::ifstream infile ("Day8Data.txt");
-
+#include <vector>
+#include <math.h>
+#include <climits>
+std::ifstream infile ("Day9Data.txt");
 using namespace std;
-int accumulator = 0;
-map<pair<int, bool>, pair<string, int>> data;
-int counter = 0;
-bool accInstBool = 1;
-bool jmpInstBool = 1;
 
-bool accInst(int acc, map<pair<int, bool>, pair<string, int>> &data) {
-	accumulator += acc;
-	//cout << "\nAdd " << acc << "\n";
+int preambleNum = 25;
+vector<long> keepPreamble(preambleNum, 0);
 
-	auto entry = data.find(make_pair(counter, 0));
-	if (entry != end(data))
-	{
-	    auto const value = std::move(entry->second);
-	    data.erase(entry);
-	    data.insert({make_pair(counter, 1), std::move(value)});
-	    counter++;
-	    return true;
-	}
-	else {
-		accInstBool = 0;
-		return false;
-	}
+void preambleArr(long a, int i) {
+	keepPreamble[i] = a;
+
+	// for(auto e : keepPreamble) {
+	// 	cout << e << " ";
+	// } cout << "\n";
 }
 
-bool jmpInst(int jmp, map<pair<int, bool>, pair<string, int>> &data) {
-	//cout << "\nJump " << jmp << "\n";
-	auto entry = data.find(make_pair(counter + jmp , 0));
-	if (entry != end(data)) 
-	{	counter = counter + jmp;
-		if(entry->second.first == "acc") {
-			accInst(entry->second.second, data);
-		}
-		else if (entry->second.first == "jmp") {
-			jmpInst(entry->second.second, data);
-		}
-		auto const value = std::move(entry->second);
-		data.erase(entry);
-		data.insert({make_pair(counter, 1), std::move(value)});
-		return true;
-	}
-	else {
-		jmpInstBool = 0;
-		return false;
-	}
-}
+long preambleSumCheck(long k) {
+	// cout << "Check " << k << " with previous array\n";
+	map<long, long> hashmap;
 
-void print(map<pair<int, bool>, pair<string, int>> &data) {
-	for (auto a : data) {
+	for (int i = 0; i < preambleNum; i++) {
+		auto search = hashmap.find(k - keepPreamble[i]);
 
-		cout << a.first.first << " " << a.first.second << " "
-		<< a.second.first << " " << a.second.second;
+		// for(auto s: hashmap){ cout << s.first << " " << s.second << "\n";}
+		// 	cout << endl;
+		if(search != hashmap.end() && search->second != i) {
+			return -1;
+		} 
+		hashmap[keepPreamble[i]] = k - keepPreamble[i];
+	} 
+	// cout << k << endl;
+	return k;
 
-		cout << "\n";
-	}
 }
 
 int main() {
 	std::string line;
-	string a;
-	int b, c;
+	long a, b, i = 0;
+	long result = INT_MIN;
 	
 	while(std::getline(infile, line)) {
 		std:istringstream iss(line);
-		if(!(iss >> a >> b >> c)) {
-			data[make_pair(counter, 0)] = make_pair(a,b);
-			counter++;
+		if(!(iss >> a >> b)) {
+			if(i < preambleNum) {
+				long temp = result;
+				result = preambleSumCheck(a);
+				result = max(temp, result);
+				preambleArr(a, i);
+				i++;
+			} 
+			else {
+				i = 0;
+				long temp = result;
+				result = preambleSumCheck(a);
+				result = max(temp, result);
+				preambleArr(a, i);
+				i++;
+			}
 		}
 	}
 
-	counter = 0;
-	// print(data);
-
-	while(accInstBool && jmpInstBool) {
-		auto a = data[make_pair(counter, 0)];
-
-		if(a.first == "acc") {
-			accInst(a.second, data);
-		}
-
-		else if (a.first == "jmp") {
-		 	jmpInst(a.second, data);
-		} 
-
-		else {
-			counter++;
-		}
-	}
-
-	// print(data);
-
-
-	cout << "Answer Q1 : "<< accumulator << "\n";	
+	cout << "Answer Q1 : "<< result << "\n";	
 }
