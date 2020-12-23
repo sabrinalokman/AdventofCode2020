202ab114a1c13a45539a7cb0932d1317de79940b
Sabrina Lokman <35489261+sabrinalokman@users.noreply.github.com>
2020-12-22 19:24:46 -0500

    Solve P1

---
+++ Day22/Day22.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+queue<int> player1;
+queue<int> player2;
+
+// void printCards() {
+// 	for (auto p1 : player1) {
+// 		cout << p1 << " ";
+// 	}
+// 	cout << endl;
+
+// 	for (auto p2 : player2) {
+// 		cout << p2 << " ";
+// 	}
+// 	cout << endl;
+// }
+
+void clearQueue(queue<int> q) {
+	while(!q.empty()) {
+		q.pop();
+	}
+}
+
+void loadFile (string file) {
+
+	clearQueue(player1);
+	clearQueue(player2);
+
+	bool anotherPlayer = false;
+
+	ifstream infile;
+	infile.open(file);
+	string line;
+	int card;
+	while (infile >> line) {
+		if(line == "Player") { }
+		else if(line == "1:") { anotherPlayer = false; }
+		else if(line == "2:") { anotherPlayer = true; }
+		else {
+			if(!anotherPlayer) {
+				player1.push(stoi(line));
+			}
+			if(anotherPlayer) {
+				player2.push(stoi(line));
+			}
+		}
+	}
+	//printCards();
+	infile.close();
+}
+
+int playCombat() {
+	while(!player1.empty() && !player2.empty()) {
+		if(player1.front() > player2.front()) {
+			player1.push(player1.front());
+			player1.push(player2.front());
+		}
+		else {
+			player2.push(player2.front());
+			player2.push(player1.front());
+		}
+		player1.pop();
+		player2.pop();
+	}
+	return player1.empty() ? 2 : 1;
+}
+
+int solution(int a) {
+	int sum = 0;
+	//Part 1
+	if (a == 1) {
+		//P1 won
+		if(playCombat() == 1) {
+			for (int i = player1.size(); i > 0; i--) {
+				sum += i * player1.front();
+				player1.pop();
+			}
+		}
+		//P2 won
+		else if(playCombat() == 2) {
+			for (int i = player2.size(); i > 0; i--) {
+				sum += i * player2.front();
+				player2.pop();
+			}
+		}
+	}
+	return sum;
+}
+
+	
+int main() {
+	
+	loadFile("Day22Data.txt");
+
+	cout << "Answer Q1 : "<< solution(1) << "\n";
+
+	// loadFile("Day11Data.txt");
+
+	// cout << "Answer Q2 : "<< solution(2) << "\n";	
+}
