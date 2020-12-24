7b9282f19819bbe393622c51f36cf67bb855f4d0
Sabrina Lokman <35489261+sabrinalokman@users.noreply.github.com>
2020-12-23 20:48:39 -0500

    Solve P1 and P2 still need to fix recursive fx

--- Day22/Day22.cpp
+++ Day22/Day22.cpp
@@ -7,6 +7,11 @@
 using namespace std;
 queue<int> player1;
 queue<int> player2;
+queue<int> temp1;
+queue<int> temp2;
+
+
+
 
 // void printCards() {
 // 	for (auto p1 : player1) {
@@ -55,6 +60,7 @@ void loadFile (string file) {
 }
 
 int playCombat() {
+
 	while(!player1.empty() && !player2.empty()) {
 		if(player1.front() > player2.front()) {
 			player1.push(player1.front());
@@ -67,6 +73,68 @@ int playCombat() {
 		player1.pop();
 		player2.pop();
 	}
+	return player1.empty() ? 2 : 1;
+}
+
+int playRecursiveCombat(queue<int> p1, queue<int> p2) {
+	
+		bool roundStart = false;
+
+		queue<int> initstate1 = p1;
+		queue<int> initstate2 = p2;
+
+	while(!player1.empty() && !player2.empty()) {
+		if(roundStart == true && 
+			initstate1 == player1 && initstate2 == player2) { 
+			return 1; 
+		}
+
+		if (player1.front() <= player1.size() - 1 
+				&& player2.front() <= player2.size() - 1) {
+
+			temp1.push(player1.front());
+			temp2.push(player2.front());
+
+			player1.pop();
+			player2.pop();
+
+			queue<int> cpyPlayer1 = player1;
+			queue<int> cpyPlayer2 = player2;
+			int subWinner = 0;
+
+			while (subWinner == 0) {
+				subWinner = playRecursiveCombat(cpyPlayer1, cpyPlayer2);
+			}
+
+			if (subWinner == 1) {
+				player1.push(temp1.front());
+				player1.push(temp2.front());
+			} 
+
+			else {
+				player2.push(temp2.front());
+				player2.push(temp1.front());
+			}
+	 
+			temp1.pop();
+			temp2.pop();
+			
+		} else {
+
+			if(player1.front() > player2.front()) {
+				player1.push(player1.front());
+				player1.push(player2.front());
+			}
+			else {
+				player2.push(player2.front());
+				player2.push(player1.front());
+			}
+			player1.pop();
+			player2.pop();
+			roundStart = true;
+		}
+	}
+
 	return player1.empty() ? 2 : 1;
 }
 
@@ -89,17 +157,35 @@ int solution(int a) {
 			}
 		}
 	}
+
+	if (a == 2) {
+		if(playRecursiveCombat(player1, player2) == 1) {
+			for (int i = player1.size(); i > 0; i--) {
+				sum += i * player1.front();
+				player1.pop();
+			}
+		}
+		//P2 won
+		else if(playRecursiveCombat(player1, player2) == 2) {
+			for (int i = player2.size(); i > 0; i--) {
+				sum += i * player2.front();
+				player2.pop();
+			}
+		}
+
+
+	}
 	return sum;
 }
 
 	
 int main() {
 	
-	loadFile("Day22Data.txt");
+	loadFile("Day22Data1.txt");
 
 	cout << "Answer Q1 : "<< solution(1) << "\n";
 
-	// loadFile("Day11Data.txt");
+	loadFile("Day22Data1.txt");
 
-	// cout << "Answer Q2 : "<< solution(2) << "\n";	
+	cout << "Answer Q2 : "<< solution(2) << "\n";	
 }
